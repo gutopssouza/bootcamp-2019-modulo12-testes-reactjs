f729d6e32380762772d437c10bab71bdefbb2b45
Augusto <gutopssouza@gmail.com>
2019-10-16 12:01:45 -0300

    :white_check_mark: Mock do useDispatch

--- src/__tests__/components/Techlist.test.js
+++ src/__tests__/components/Techlist.test.js
@@ -1,6 +1,8 @@
 import React from "react";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import { render, fireEvent, cleanup } from "@testing-library/react";
+
+import { addTech } from "~/store/modules/techs/actions";
 import Techlist from "~/components/Techlist";
 
 jest.mock("react-redux");
@@ -17,5 +19,20 @@ describe("Techlist", () => {
 
     expect(getByTestId("tech-list")).toContainElement(getByText("Node.js"));
     expect(getByTestId("tech-list")).toContainElement(getByText("ReactJS"));
+  });
+
+  it("should be able to add new tech", () => {
+    const { getByTestId, getByLabelText } = render(<Techlist />);
+
+    const dispatch = jest.fn();
+
+    useDispatch.mockReturnValue(dispatch);
+
+    fireEvent.change(getByLabelText("Tech"), { target: { value: "Node.js" } });
+    fireEvent.submit(getByTestId("tech-form"));
+
+    console.log(dispatch.mock.calls);
+
+    expect(dispatch).toHaveBeenCalledWith(addTech("Node.js"));
   });
 });
