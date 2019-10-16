a4db65805b186f462dd759d38f8ffc22e90c29b6
Augusto <gutopssouza@gmail.com>
2019-10-16 11:27:05 -0300

    :white_check_mark: Mock do LocalStorage :white_check_mark: Implment test using localstorage mock

--- src/__tests__/components/Techlist.test.js
+++ src/__tests__/components/Techlist.test.js
@@ -1,8 +1,11 @@
 import React from "react";
-import { render, fireEvent } from "@testing-library/react";
+import { render, fireEvent, cleanup } from "@testing-library/react";
 import Techlist from "~/components/Techlist";
 
 describe("Techlist", () => {
+  beforeEach(() => {
+    localStorage.clear();
+  });
   it("should be able to add new tech", () => {
     const { getByText, getByTestId, getByLabelText } = render(<Techlist />);
 
@@ -11,5 +14,22 @@ describe("Techlist", () => {
 
     expect(getByTestId("tech-list")).toContainElement(getByText("Node.js"));
     expect(getByLabelText("Tech")).toHaveValue("");
+  });
+
+  it("should store techs in storage", () => {
+    let { getByText, getByTestId, getByLabelText } = render(<Techlist />);
+
+    fireEvent.change(getByLabelText("Tech"), { target: { value: "Node.js" } });
+    fireEvent.submit(getByTestId("tech-form"));
+
+    cleanup();
+
+    ({ getByText, getByTestId, getByLabelText } = render(<Techlist />));
+
+    expect(localStorage.setItem).toHaveBeenCalledWith(
+      "techs",
+      JSON.stringify(["Node.js"])
+    );
+    expect(getByTestId("tech-list")).toContainElement(getByText("Node.js"));
   });
 });
